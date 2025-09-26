01865f2846e6ebd0fa592f7049ba43db1f71e362
Erik Beitel <Erik.Beitel@gmail.com>
2025-09-26 11:23:45 -0500

    finished the data load

--- Assignment 2/Assignment_2/assignment_2.py
+++ Assignment 2/Assignment_2/assignment_2.py
@@ -1,3 +1,4 @@
+import csv
 def load_csv(file_name):
     """Opens and reads the provided file. Creates a dictionary that maps name to a
         list of ranking and value. For example, the "population" dictionary would look like...
@@ -15,8 +16,24 @@ def load_csv(file_name):
         Returns:
             A dictionary mapping 'name' to a list containing rank and value - both as strings. 
         """
+    features = {}
     #write your code here
-    return None
+    with open(file_name, 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        for row in reader: #Read the data e.g. "Burkina Faso","burkina-faso","-3.20","2019 est.","1","Africa"
+            country = row[0].lower()
+            rank = row[4]
+            value = row[2]
+            region = row[5].lower()
+            date_of_info = row[3].lower()
+
+            #format the the data e.g. "Burkina Faso": ["1", "-3.20", "2019 est.", "1", "Africa"] e.g {[0]: [[4],[2]], [1]: [[5],[3]]    })}
+            features[country] = [rank, value, region, date_of_info]
+
+
+        #return the formatted data
+    return features
 
 features = {}
 features["area"] = load_csv("world_factbook/geography/area.csv")
@@ -30,4 +47,6 @@ assert features["life expectancy"]["chile"][1] == "79.57", "Chile life expectanc
 assert features["area"]["south africa"][1] == "1,219,090", "South Africa area value test"
 assert len(features["median age"]) == 226, "Median Age dictionary size test"
 assert features["gdp"]["china"][0] == "1", "China gdp rank test"
-
+assert features["gdp"]["china"][3] == "2019 est.", "China gdp date of info test"
+assert features["population"]["china"][2] == "east asia/southeast asia", "China region test"
+print("if we get here, the tests passed")
